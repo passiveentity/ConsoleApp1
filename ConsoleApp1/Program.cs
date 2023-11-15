e19ce026e227f58552a321c4904bf13685157ac8
User <User@COMPUTER>
2023-11-15 19:38:19 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+
+class Program
+
+{
+    static void Main()
+
+    {
+        Console.WriteLine("Введите значение a:");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите значение b:");
+        double b = Convert.ToDouble(Console.ReadLine());
+        double result;
+
+        if (a > b)
+        {
+            result = a / b - 1;
+        }
+
+        else if (a < b)
+        {
+            result = (Math.Pow(a, 3) - 5) / a;
+        }
+
+        else
+        {
+            result = -25;
+        }
+
+        Console.WriteLine($"Результат X: {result}");
+
+    }
+}
